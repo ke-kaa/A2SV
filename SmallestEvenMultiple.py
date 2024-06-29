1dc83a8d906a653eaaf40c8ee819bd0091314609
Kakuu <139550467+ke-kaa@users.noreply.github.com>
2024-06-29 17:56:50 +0300

    Create SmallestEvenMultiple.py

---
+++ SmallestEvenMultiple.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def smallestEvenMultiple(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n%2 == 0: return n
+        return n*2
